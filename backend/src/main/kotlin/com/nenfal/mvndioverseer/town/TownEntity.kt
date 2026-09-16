baca75c26edc2ac215ff8079ac6a5429b34fdb0d
MatthieuClaessens <matthieu-claessens@hotmail.com>
2026-09-16 16:44:56 +0200

    Created TownEntity.kt

---
+++ backend/src/main/kotlin/com/nenfal/mvndioverseer/town/TownEntity.kt
@@ -0,0 +1,24 @@
+package com.nenfal.mvndioverseer.town
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.math.BigDecimal
+import java.util.UUID
+
+@Entity
+@Table(name = "towns")
+class TownEntity(
+    @Id
+    @Column(name = "town_id", updatable = false, nullable = false)
+    val townId: UUID,
+
+    @Column(name = "town_name", nullable = false)
+    var townName: String,
+
+    @Column(name = "town_rate", precision = 3, scale = 2)
+    var townRate: BigDecimal? = null
+) {
+    constructor() : this(UUID.randomUUID(), "", null)
+}
