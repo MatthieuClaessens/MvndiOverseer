8933790c86099d9669210ab24e4df93589187bd6
MatthieuClaessens <matthieu-claessens@hotmail.com>
2026-09-16 16:04:27 +0200

    Project initialization

---
+++ backend/src/main/kotlin/com/nenfal/mvndioverseer/MvndiOverseerApplication.kt
@@ -0,0 +1,11 @@
+package com.nenfal.mvndioverseer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MvndiOverseerApplication
+
+fun main(args: Array<String>) {
+    runApplication<MvndiOverseerApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/nenfal/mvndioverseer/MvndiOverseerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nenfal.mvndioverseer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MvndiOverseerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
